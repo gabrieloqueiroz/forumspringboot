0f9ba5c93eb693fc50ba71288088e758ab1388e3
Gabriel Queiroz <gabriel-cso@hotmail.com>
2022-04-17 21:57:17 -0300

    Adicionando teste automatizado utilizando Jupiter

---
+++ src/test/java/br/com/alura/forum/repository/CourseRepositoryTest.java
@@ -0,0 +1,31 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.model.Course;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class CourseRepositoryTest {
+
+    private CourseRepository courseRepository;
+
+    @Autowired
+    public CourseRepositoryTest(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @Test
+    public void should_return_course_when_find_by_name(){
+        //Given
+        String courseName = "HTML 5";
+
+        //When
+        Course response = courseRepository.findByName(courseName);
+
+        //Then
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(courseName, response.getName());
+    }
+}
